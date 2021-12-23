370d81c5e26d82f2c3e3cfcefde931cb369b3c22
Leeyo Rapirap <leorapirap@gmail.com>
2021-12-23 16:42:11 +0800

    create tasks table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
 import React from "react";
+import '../styles/main.css';
+
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <table className="tasks">
+                <tr>
+                    <th>Header 1</th>
+                    <th>Header 2</th>
+                    <th>Header 3</th>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                    <td>Lorem ipsum dolor sit.</td>
+                </tr>
+            </table>
         </>
     )
 }
